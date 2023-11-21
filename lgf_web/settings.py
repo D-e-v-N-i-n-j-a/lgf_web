36e96f403b0e0c7e80f5358172f9ff317bec79fe
webblyss <skyhightech93@gmail.com>
2023-11-21 08:28:50 +0000

    productioin set to false

--- lgf_web/settings.py
+++ lgf_web/settings.py
@@ -23,7 +23,7 @@ import os
 SECRET_KEY = 'django-insecure-n7az+gms)lg+zfu1jt(6)t3=m*s+_vfc_x%b7nnw1luo3pd951'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['13.48.130.71','learnersgirlsfoundation.org','127.0.0.1']
 
